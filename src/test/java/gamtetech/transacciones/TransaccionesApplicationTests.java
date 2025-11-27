230c4c2e07bf79c68da6ea5d92da167aa8c0f338
felipesalazar24 <fe.salazarv@duocuc.cl>
2025-11-27 16:42:06 -0300

    Add files via upload

---
+++ src/test/java/gamtetech/transacciones/TransaccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package gamtetech.transacciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransaccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
